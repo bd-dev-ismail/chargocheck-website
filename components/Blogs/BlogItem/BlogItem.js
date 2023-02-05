c40905aff475aa541ec31b181dfd8a59703840ae
sainijagjit <sainijagjit@rocketmail.com>
2023-02-05 13:20:25 +0530

    minor change

--- components/Blogs/BlogItem/BlogItem.js
+++ components/Blogs/BlogItem/BlogItem.js
@@ -1,53 +1,40 @@
-import img1 from "/assets/blog/image 10.png";
-import img2 from "/assets/blog/image 10 (1).png";
-import img3 from "/assets/blog/image 10 (2).png";
-import img4 from "/assets/blog/image 10 (3).png";
-import img5 from "/assets/blog/image 10 (4).png";
-import img6 from "/assets/blog/image 10 (5).png";
-import icon from "/assets/icon/shareIcon.png";
-import { useEffect, useState } from "react";
-import Link from "next/link";
+import img1 from '/assets/blog/image 10.png';
+import img2 from '/assets/blog/image 10 (1).png';
+import img3 from '/assets/blog/image 10 (2).png';
+import img4 from '/assets/blog/image 10 (3).png';
+import img5 from '/assets/blog/image 10 (4).png';
+import img6 from '/assets/blog/image 10 (5).png';
+import icon from '/assets/icon/shareIcon.png';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 const BlogItem = () => {
   const [blogs, setBlogs] = useState([]);
- 
+
   useEffect(() => {
-    fetch("http://40.76.241.63/api/blogs?populate=*")
+    fetch('http://cms.cargocheck.com/api/blogs?populate=*')
       .then((res) => res.json())
       .then((data) => setBlogs(data.data));
   }, []);
-  console.log(blogs, "blog");
-  
+  console.log(blogs, 'blog');
+
   return (
     <>
       {blogs.map((item, idx) => (
-        <Link href={"/blog-details"} key={idx}>
+        <Link href={'/blog-details'} key={idx}>
           <div className="card card-compact my-5 relative w-96 bg-base-100 shadow-xl">
             <figure>
-              <img
-                src={`http://cms-cargocheck.eastus.cloudapp.azure.com${item?.attributes?.dispayImage?.data?.attributes?.url}`}
-              />
+              <img src={`http://cms-cargocheck.eastus.cloudapp.azure.com${item?.attributes?.dispayImage?.data?.attributes?.url}`} />
             </figure>
             <div className="card-body">
-              <p className="text-secondary font-light mb-2">
-                {item?.attributes?.readTime} read
-              </p>
-              <h2
-                className="card-title font-bold pb-2 text-primary"
-                style={{ fontSize: "2rem" }}
-              >
+              <p className="text-secondary font-light mb-2">{item?.attributes?.readTime} read</p>
+              <h2 className="card-title font-bold pb-2 text-primary" style={{ fontSize: '2rem' }}>
                 {item.attributes?.title}
               </h2>
-              <p className="text-secondary font-light text-xl">
-                {item.attributes?.shortDescription}
-              </p>
+              <p className="text-secondary font-light text-xl">{item.attributes?.shortDescription}</p>
               <p className="text-secondary font-bold py-6">Jan, 01 2023</p>
               <div>
-                <img
-                  src={icon.src}
-                  alt=""
-                  className="absolute top-[5%] right-5"
-                />
+                <img src={icon.src} alt="" className="absolute top-[5%] right-5" />
               </div>
             </div>
           </div>
--- components/FAQ/FaqCollapse/FaqCollapse.js
+++ components/FAQ/FaqCollapse/FaqCollapse.js
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react";
-import { FaMinus, FaPlus } from "react-icons/fa";
+import { useEffect, useState } from 'react';
+import { FaMinus, FaPlus } from 'react-icons/fa';
 const FaqCollapse = () => {
   const [open, SetIsOpen] = useState(true);
   const [faqs, setFaqs] = useState([]);
   const faq = [
     {
-      name: "Why ChargoCheck?",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.",
+      name: 'Why ChargoCheck?',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.',
     },
     {
-      name: "How We Work at ChargoCheck?",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.",
+      name: 'How We Work at ChargoCheck?',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.',
     },
     {
-      name: "What is the different industries we target?",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.",
+      name: 'What is the different industries we target?',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.',
     },
     {
-      name: "Is ChargoCheck Secure?",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.",
+      name: 'Is ChargoCheck Secure?',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.',
     },
     {
-      name: "How ChargoCheck is better?",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.",
+      name: 'How ChargoCheck is better?',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, aliquid suscipit dolor nulla corrupti voluptate? Soluta, eos veritatis ab nihil, reiciendis deserunt qui quam quas aliquid reprehenderit iure? Doloremque animi blanditiis accusamus excepturi aperiam. Cupiditate, iste. Adipisci neque quia voluptatibus possimus. Voluptates reprehenderit nisi tenetur. Nemo repellendus illo nobis iusto.',
     },
   ];
   useEffect(() => {
-    fetch("http://40.76.241.63/api/faqs")
+    fetch('https://cms.cargocheck.com/api/faqs')
       .then((res) => res.json())
       .then((data) => setFaqs(data.data));
   }, []);
-  
+
   return (
     <div className="my-[8rem] sm:max-w-xl md:max-w-full lg:max-w-screen-xl mx-auto">
       <section className="dark:bg-gray-800 dark:text-gray-100 bg-white">
@@ -44,14 +44,12 @@ const FaqCollapse = () => {
                     <summary
                       onClick={() => SetIsOpen(!open)}
                       className="px-4 py-4  cursor-pointer focus:outline-none text-primary font-bold flex items-center justify-between"
-                      style={{ fontSize: "24px" }}
+                      style={{ fontSize: '24px' }}
                     >
                       {item?.attributes?.question}
                       {open ? <FaPlus /> : <FaMinus />}
                     </summary>
-                    <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-secondary text-lg font-light">
-                      {item?.attributes?.answer}
-                    </p>
+                    <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-secondary text-lg font-light">{item?.attributes?.answer}</p>
                   </details>
                 )
               )
